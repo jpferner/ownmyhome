946c3aa2d19b104898385076335705b7d43f083a
Sage5tar <90434064+Sage5tar@users.noreply.github.com>
2023-02-21 20:24:13 -0500

    Update CheckList.py

--- CheckList.py
+++ CheckList.py
@@ -1,13 +1,14 @@
 class CheckList:
     user_ID: int
     steps: list
-    def __int__(self, user: int, steps: list):
+    user_steps: list
+    def __int__(self, user: int, steps: list, user_steps):
         self.user_ID = user
         self.steps = steps
-
+        self.user_steps = user_steps
 
 SAMPLE_CHECKLIST = [
-    CheckList(111111, ["step 1", "step 2", "step 3"]),
-    CheckList(222222, ["step 1", "step 2", "step 3"]),
-    CheckList(333333, ["step 1", "step 2", "step 3"])
+    CheckList(111111, ["step 1", "step 2", "step 3"], [True, True, False]),
+    CheckList(222222, ["step 1", "step 2", "step 3"], [True, True, True]),
+    CheckList(333333, ["step 1", "step 2", "step 3"], [True, False, False])
 ]
