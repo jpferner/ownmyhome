ea71c867929c5a4825966390253d8e21aee90a05
Mark Karels <mak8966@uncw.edu>
2023-02-27 13:52:32 -0500

    Pulling Data from Python List
    The table is now updating from the python list in the checklistitems.py file, and when you click complete it is sending it to the completed list, but I'm having trouble getting the undo button to show up properly

--- app.py
+++ app.py
@@ -1,24 +1,14 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, jsonify
+
+import ChecklistItems
+
 app = Flask(__name__)
 
-# TASK = [
-#     {
-#         'id': 1,
-#         'step': 'First',
-#         'title': 'Check Your Credit Scores',
-#         'complete': False
-#     },
-#     {
-#         'id': 2,
-#         'step': 'Second',
-#         'title': 'Calculate A Budget',
-#         'complete': False
-#     }
-# ]
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route('/properties', methods=['GET', 'POST'])
 def properties():
@@ -26,11 +16,35 @@ def properties():
         return redirect(url_for('index'))
     return render_template('properties.html')
 
+
 @app.route('/checklist', methods=['GET', 'POST'])
 def checklist():
     if request.method == 'POST':
         return redirect(url_for('index'))
-    return render_template('checklist.html')
+    return render_template('checklist.html', checklist_items=ChecklistItems.checklist_items)
+
+
+@app.route('/complete-item', methods=['POST'])
+def complete_item():
+    data = request.get_json()
+    item_id = data['id']
+    for item in ChecklistItems.checklist_items:
+        if item.order_no == int(item_id):
+            item.mark_completed()
+            return jsonify({'success': True})
+    return jsonify({'success': False})
+
+
+@app.route('/undo-item', methods=['POST'])
+def undo_item():
+    data = request.get_json()
+    item_id = data['id']
+    for item in ChecklistItems.checklist_items:
+        if item.order_no == item_id:
+            item.completed = False
+            return jsonify({'success': True})
+    return jsonify({'success': False})
+
 
 @app.route('/calendar', methods=['GET', 'POST'])
 def calendar():
@@ -38,11 +52,13 @@ def calendar():
         return redirect(url_for('index'))
     return render_template('calendar.html')
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         return redirect(url_for('index'))
     return render_template('login.html')
+
 
 @app.route('/calculator', methods=['GET', 'POST'])
 def calculator():
@@ -50,17 +66,20 @@ def calculator():
         return redirect(url_for('index'))
     return render_template('calculator.html')
 
+
 @app.route('/services', methods=['GET', 'POST'])
 def services():
     if request.method == 'POST':
         return redirect(url_for('index'))
     return render_template('services.html')
 
+
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         return redirect(url_for('index'))
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
--- static/scripts/checklist.js
+++ static/scripts/checklist.js
@@ -1,56 +1,68 @@
-const todoList = document.getElementById('todo-list');
-const completedList = document.getElementById('completed-list');
-
-todoList.addEventListener('click', function(event) {
-  if (event.target.classList.contains('complete-btn')) {
-    const confirmMessage = 'Are you sure you want to complete this item?';
-    if (confirm(confirmMessage)) {
-      const listItem = event.target.parentElement;
-      todoList.removeChild(listItem);
-      const completeButton = listItem.querySelector('.complete-btn');
-      listItem.removeChild(completeButton);
-      const undoButton = document.createElement('button');
-      undoButton.innerText = 'Undo';
-      undoButton.classList.add('undo-btn');
-      listItem.appendChild(undoButton);
-      const items = Array.from(completedList.querySelectorAll('li'));
-      items.push(listItem);
-      items.sort((a, b) => {
-        const aStep = parseInt(a.querySelector('.step').innerText.slice(5));
-        const bStep = parseInt(b.querySelector('.step').innerText.slice(5));
-        return aStep - bStep;
-      });
-      completedList.innerHTML = '';
-      items.forEach(item => {
-        completedList.appendChild(item);
-      });
-    }
-  }
+// Add click event listener to complete buttons
+document.querySelectorAll('.complete-btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const id = btn.dataset.id;
+    const row = btn.closest('tr');
+    const data = {id: id};
+    fetch('/complete-item', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.success) {
+        // Update row in To-Do table
+        row.querySelector('.status').textContent = 'Completed';
+        row.querySelector('.complete-btn').remove();
+        // Move row to Completed table
+        const completedList = document.querySelector('#completed-list');
+        completedList.appendChild(row);
+      } else {
+        alert('Error completing item. Please try again later.');
+      }
+    })
+    .catch(error => {
+      console.error('Error completing item:', error);
+      alert('Error completing item. Please try again later.');
+    });
+  });
 });
 
-completedList.addEventListener('click', function(event) {
-  if (event.target.classList.contains('undo-btn')) {
-    const confirmMessage = 'Are you sure you want to undo this item?';
-    if (confirm(confirmMessage)) {
-      const listItem = event.target.parentElement;
-      completedList.removeChild(listItem);
-      const undoButton = listItem.querySelector('.undo-btn');
-      listItem.removeChild(undoButton);
-      const completeButton = document.createElement('button');
-      completeButton.innerText = 'Complete';
-      completeButton.classList.add('complete-btn');
-      listItem.appendChild(completeButton);
-      const items = Array.from(todoList.querySelectorAll('li'));
-      items.push(listItem);
-      items.sort((a, b) => {
-        const aStep = parseInt(a.querySelector('.step').innerText.slice(5));
-        const bStep = parseInt(b.querySelector('.step').innerText.slice(5));
-        return aStep - bStep;
-      });
-      todoList.innerHTML = '';
-      items.forEach(item => {
-        todoList.appendChild(item);
-      });
-    }
-  }
+// Add click event listener to undo buttons
+document.querySelectorAll('.undo-btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const id = btn.dataset.id;
+    const row = btn.closest('tr');
+    const data = {id: id};
+    fetch('/undo-item', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.success) {
+        // Update row in Completed table
+        row.querySelector('.status').textContent = 'Incomplete';
+        row.querySelector('.undo-btn').remove();
+        // Move row to To-Do table
+        const todoList = document.querySelector('#todo-list');
+        todoList.appendChild(row);
+        // Add complete button to row
+        const completeBtn = document.createElement('button');
+        completeBtn.classList.add('complete-btn');
+        completeBtn.dataset.id = id;
+        completeBtn.textContent = 'Complete';
+        row.querySelector('td:last-child').appendChild(completeBtn);
+      } else {
+        alert('Error undoing item. Please try again later.');
+      }
+    })
+    .catch(error => {
+      console.error('Error undoing item:', error);
+      alert('Error undoing item. Please try again later.');
+    });
+  });
 });
+
