e350a23e95d67c220d9839a61cee15d0e770443d
Mark Karels <mak8966@uncw.edu>
2023-02-27 15:36:58 -0500

    Now Working Both Ways

--- static/scripts/checklist.js
+++ static/scripts/checklist.js
@@ -1,68 +1,86 @@
-// Add click event listener to complete buttons
-document.querySelectorAll('.complete-btn').forEach(btn => {
-  btn.addEventListener('click', () => {
-    const id = btn.dataset.id;
-    const row = btn.closest('tr');
-    const data = {id: id};
-    fetch('/complete-item', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(data => {
-      if (data.success) {
-        // Update row in To-Do table
-        row.querySelector('.status').textContent = 'Completed';
-        row.querySelector('.complete-btn').remove();
-        // Move row to Completed table
-        const completedList = document.querySelector('#completed-list');
+// Get the To-Do and Completed tables
+const todoList = document.getElementById('todo-list');
+const completedList = document.getElementById('completed-list');
+
+// Add event listeners to the Complete and Undo buttons
+const completeButtons = document.querySelectorAll('.complete-btn');
+completeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const row = button.parentNode.parentNode;
+        const id = row.getAttribute('data-id');
+        const statusCell = row.querySelector('.status');
+
+        // Update the status and move the row to the Completed table
+        statusCell.textContent = 'Completed';
+        todoList.removeChild(row);
         completedList.appendChild(row);
-      } else {
-        alert('Error completing item. Please try again later.');
-      }
-    })
-    .catch(error => {
-      console.error('Error completing item:', error);
-      alert('Error completing item. Please try again later.');
-    });
-  });
-});
 
-// Add click event listener to undo buttons
-document.querySelectorAll('.undo-btn').forEach(btn => {
-  btn.addEventListener('click', () => {
-    const id = btn.dataset.id;
-    const row = btn.closest('tr');
-    const data = {id: id};
-    fetch('/undo-item', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(data => {
-      if (data.success) {
-        // Update row in Completed table
-        row.querySelector('.status').textContent = 'Incomplete';
-        row.querySelector('.undo-btn').remove();
-        // Move row to To-Do table
-        const todoList = document.querySelector('#todo-list');
-        todoList.appendChild(row);
-        // Add complete button to row
-        const completeBtn = document.createElement('button');
-        completeBtn.classList.add('complete-btn');
-        completeBtn.dataset.id = id;
-        completeBtn.textContent = 'Complete';
-        row.querySelector('td:last-child').appendChild(completeBtn);
-      } else {
-        alert('Error undoing item. Please try again later.');
-      }
-    })
-    .catch(error => {
-      console.error('Error undoing item:', error);
-      alert('Error undoing item. Please try again later.');
-    });
-  });
-});
+        // Add the Undo button
+        const undoButton = document.createElement('button');
+        undoButton.textContent = 'Undo';
+        undoButton.setAttribute('data-id', id);
+        undoButton.classList.add('undo-btn');
+        const actionCell = button.parentNode;
+        actionCell.appendChild(undoButton);
 
+// Remove the Complete button and disable it
+        button.remove();
+
+// Add event listener to the Undo button
+        undoButton.addEventListener('click', () => {
+            const row = undoButton.parentNode.parentNode;
+            const id = row.getAttribute('data-id');
+            const statusCell = row.querySelector('.status');
+
+            // Update the status and move the row back to the To-Do table
+            statusCell.textContent = 'Incomplete';
+            completedList.removeChild(row);
+            todoList.appendChild(row);
+
+            // Remove the Undo button and add the Complete button back
+            undoButton.remove();
+            const completeButton = document.createElement('button');
+            completeButton.textContent = 'Complete';
+            completeButton.setAttribute('data-id', id);
+            completeButton.classList.add('complete-btn');
+            actionCell.appendChild(completeButton);
+
+            // Add event listener to the Complete button
+            completeButton.addEventListener('click', () => {
+                const row = completeButton.parentNode.parentNode;
+                const id = row.getAttribute('data-id');
+                const statusCell = row.querySelector('.status');
+
+                // Update the status and move the row to the Completed table
+                statusCell.textContent = 'Completed';
+                todoList.removeChild(row);
+                completedList.appendChild(row);
+
+                // Add the Undo button and remove the Complete button
+                const undoButton = document.createElement('button');
+                undoButton.textContent = 'Undo';
+                undoButton.setAttribute('data-id', id);
+                undoButton.classList.add('undo-btn');
+                actionCell.appendChild(undoButton);
+                completeButton.remove();
+
+                // Add event listener to the Undo button
+                undoButton.addEventListener('click', () => {
+                    const row = undoButton.parentNode.parentNode;
+                    const id = row.getAttribute('data-id');
+                    const statusCell = row.querySelector('.status');
+
+                    // Update the status and move the row back to the To-Do table
+                    statusCell.textContent = 'Incomplete';
+                    completedList.removeChild(row);
+                    todoList.appendChild(row);
+
+                    // Remove the Undo button and add the Complete button back
+                    undoButton.remove();
+                    actionCell.appendChild(completeButton);
+                });
+            });
+        });
+
+    });
+});
