b2220aa31da51a321b9573460d7335951140484e
Sage5tar <90434064+Sage5tar@users.noreply.github.com>
2023-04-07 01:07:18 -0400

    Debt calc + minor edits

--- app/static/scripts/calculator.js
+++ app/static/scripts/calculator.js
@@ -3,6 +3,7 @@ function validate(event){
 
     let check;
     let test = true;
+
     check = document.querySelector('input[id="HomeVal"]');
     console.log(check.checkValidity())
     if (!check.checkValidity()){check.reportValidity(); test = false}
@@ -15,6 +16,14 @@ function validate(event){
     check = document.querySelector('input[id="LoanTerm"]');
     if (!check.checkValidity()){check.reportValidity(); test = false}
     check = document.querySelector('input[id="PropTax"]');
+    if (!check.checkValidity()){check.reportValidity(); test = false}
+    check = document.querySelector('input[id="Income"]');
+    if (!check.checkValidity()){check.reportValidity(); test = false}
+    check = document.querySelector('input[id="Credit"]');
+    if (!check.checkValidity()){check.reportValidity(); test = false}
+    check = document.querySelector('input[id="CarPay"]');
+    if (!check.checkValidity()){check.reportValidity(); test = false}
+    check = document.querySelector('input[id="StudentPay"]');
     if (!check.checkValidity()){check.reportValidity(); test = false}
 
     if (test) {
@@ -40,26 +49,40 @@ function calculation(){
     const interest = document.getElementById('InterestRate').value / 100;
     const loanTerm = document.getElementById('LoanTerm').value;
     const prop = document.getElementById('PropTax').value;
-    const loanType = document.getElementById('LoanType').value;
+    const loan_type = document.getElementById('LoanType').value;
+    const an_income = document.getElementById('Income').value;
+    const credit = document.getElementById('Credit').value;
+    const carPay = document.getElementById('CarPay').value;
+    const studentPay = document.getElementById('StudentPay').value;
+
+    const mon_income = an_income / 12;
+    const mon_payments = (Number(credit) + Number(carPay) + Number(studentPay)) * 100
+
     const loanTermMonth = loanTerm * 12;
     const mon_int = interest / 12;
     const mon_prop_pay = prop / 12;
 
+    console.log(mon_payments)
+    console.log(mon_income)
 
-
+    // Debt to income
+    let debt_income_percent = (mon_payments / mon_income)
+    console.log(debt_income_percent)
     // Fixed Rate Loan
     let mort_Mon_Total = (loan * mon_int * Math.pow(1 + mon_int, loanTermMonth)) / (Math.pow(1 + mon_int, loanTermMonth) - 1);
     let total_mort = mort_Mon_Total * loanTermMonth;
 
     // Adds Property Tax onto monthly total
-    mort_Mon_Total += mon_prop_pay
+    let total_mon_payment= mort_Mon_Total + mon_prop_pay
 
     let mort_interest = total_mort - loan
     total_mort = total_mort + (prop * 30)
 
+    document.getElementById('MonPayTotal').textContent=Number(total_mon_payment.toFixed(2)).toLocaleString()
     document.getElementById('MortMonTotal').textContent=Number(mort_Mon_Total.toFixed(2)).toLocaleString()
     document.getElementById('TotalMort').textContent=Number(total_mort.toFixed(2)).toLocaleString()
     document.getElementById('TotalInterest').textContent=Number(mort_interest.toFixed(2)).toLocaleString()
+    document.getElementById('Debt').textContent=Number(debt_income_percent.toFixed(2)).toLocaleString()
 
     return false
 }
