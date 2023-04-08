984bdeb291661eccf1760b1e3bcc78d7f2ecf53f
Sage5tar <90434064+Sage5tar@users.noreply.github.com>
2023-04-08 17:21:58 -0400

    Math Error Fix + edits

--- app/static/scripts/calculator.js
+++ app/static/scripts/calculator.js
@@ -34,16 +34,12 @@ function validateAll(event){
     if (test) {
         calculation()
     }
-    return false
-
-
 }
 
 function validateInput(input) {
   const valid = input.checkValidity();
   if (!valid) {
     input.reportValidity();
-    return false;
   }
 }
 function calculation(){
@@ -83,7 +79,7 @@ function calculation(){
 
     // Debt to income
     let debt_income_percent = (mon_payments / mon_income) * 100
-    let debt_income_percent_mort = ((mon_payments + mort_Mon_Total) / mon_income) * 100
+    let debt_income_percent_mort = (total_mon_payment / mon_income) * 100
 
     document.getElementById('MonPayTotal').textContent=Number(total_mon_payment.toFixed(2)).toLocaleString()
     document.getElementById('MortMonTotal').textContent=Number(mort_Mon_Total.toFixed(2)).toLocaleString()
@@ -91,6 +87,4 @@ function calculation(){
     document.getElementById('TotalInterest').textContent=Number(mort_interest.toFixed(2)).toLocaleString()
     document.getElementById('Debt').textContent=Number(debt_income_percent.toFixed(2)).toLocaleString()
     document.getElementById('DebtMort').textContent=Number(debt_income_percent_mort.toFixed(2)).toLocaleString()
-
-    return false
 }
