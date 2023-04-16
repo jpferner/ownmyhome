6b6ce7e271e2b47fdb70abf57aea6867e439e62e
Mark Karels <mak8966@uncw.edu>
2023-04-16 12:17:59 -0400

    updated to a static secret key

--- config.py
+++ config.py
@@ -7,8 +7,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Random string (key) used to secure the sessions that remember info from one request to another
-    SECRET_KEY = os.urandom(24)  # generate a string of random characters of size 24
+    SECRET_KEY = 'b23d23194d7061782f754963d11e0cb50a9aa051605d891b1bb94594f209362b'
 
     # Configuring Flask-Mail
     MAIL_SERVER = "smtp.gmail.com"  # Google SMTP server address
