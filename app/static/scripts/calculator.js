47f6fe76377736f73b76e66f255ad57ad58f7cca
Sage5tar <90434064+Sage5tar@users.noreply.github.com>
2023-04-20 14:16:25 -0400

    Added down payment percentage output

--- app/static/scripts/calculator.js
+++ app/static/scripts/calculator.js
@@ -156,6 +156,8 @@ function calculation(){
     let debt_income_percent_mort = (total_mon_payment / mon_income) * 100
     let debt_income_mort_budget = mon_income - total_mon_payment
 
+    let downpaypercent = (down / home) * 100
+
     document.getElementById('MonPayTotal').textContent=Number(total_mon_payment.toFixed(2)).toLocaleString()
     document.getElementById('MortMonTotal').textContent=Number(mort_Mon_Total.toFixed(2)).toLocaleString()
     document.getElementById('TotalMort').textContent=Number(total_mort.toFixed(2)).toLocaleString()
@@ -164,4 +166,6 @@ function calculation(){
     document.getElementById('DebtBudget').textContent=Number(debt_income_budget.toFixed(2)).toLocaleString()
     document.getElementById('DebtMort').textContent=Number(debt_income_percent_mort.toFixed(2)).toLocaleString()
     document.getElementById('DebtMortBudget').textContent=Number(debt_income_mort_budget.toFixed(2)).toLocaleString()
+    document.getElementById('DownPayPercent').textContent=Number(downpaypercent.toFixed(2)).toLocaleString()
+
 }
